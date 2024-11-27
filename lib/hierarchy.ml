85991c593e852575a6a3851266aa371239ace1ec
Roy Ang <ra652@cam.ac.uk>
2024-11-27 18:20:28 +0000

    changed overpass query to sequential due to rate limits, added admin_level to osm files, fixed hierarchy building to visit each boundary only once

--- lib/hierarchy.ml
+++ lib/hierarchy.ml
@@ -2,16 +2,26 @@ open Core
 
 module StringMap = Map.Make(String)
 
-let rec build_hierarchy_helper (root : string) parent_mp=
-  let osm = Osm_xml.Parser.parse_file ("data/boundaries/"^root^".osm") in
+exception TagNotFound of string
+
+(** given a root, get its children boundaries from the osm file, set parent_mp[child]=root and recurse down child if first time seen*)
+let rec build_hierarchy_helper (root : string) (admin_level : string) parent_mp=
+  let osm = Osm_xml.Parser.parse_file ("data/boundaries/"^admin_level^"-"^root^".osm") in
   let relations_of_osm (Osm_xml.Types.OSM osm_record) = osm_record.relations in
   let relations = Map.to_alist (relations_of_osm osm) in
   let rec explore_children list mp=
   match list with
   | [] -> mp
-  | (Osm_xml.Types.OSMId child ,Osm_xml.Types.OSMRelation _)::xs -> explore_children xs (build_hierarchy_helper child (Map.set mp ~key:child ~data:root)) in
+  | (Osm_xml.Types.OSMId child ,Osm_xml.Types.OSMRelation osm_relation_record)::xs -> let a_level= Osm_xml.Types.find_tag osm_relation_record.tags "admin_level" in
+    match a_level with
+    | None -> raise (TagNotFound child)
+    | Some level -> match Map.mem mp (level^"-"^child) with
+      | true  -> explore_children xs (Map.set mp ~key:(level^"-"^child) ~data:(admin_level^"-"^root))
+      | false -> explore_children xs (build_hierarchy_helper child level (Map.add_exn mp ~key:(level^"-"^child) ~data:(admin_level^"-"^root)))
+  in
   explore_children relations parent_mp
 
-let build_hierarchy (root : string) = 
-  build_hierarchy_helper root StringMap.empty
+(** given a root, return a map of key child and value parent for all boundaries contained in root. mp[root]="0-root"*)
+let build_hierarchy (root : string) (admin_level : string)= 
+  build_hierarchy_helper root admin_level (Map.add_exn StringMap.empty ~key:(admin_level^"-"^root) ~data:"0-root")
 
--- lib/overpass.ml
+++ lib/overpass.ml
@@ -3,35 +3,48 @@ open Cohttp_lwt
 open Cohttp_lwt_unix
 open Core
 
-let download (uri : Uri.t) (dest : string) =
-  let* _resp, body = Client.get uri in
-  let stream = Body.to_stream body in
+let api = "https://overpass-api.de/api/interpreter"
+(*"https://overpass.osm.jp/api/interpreter"*)
+(*"https://maps.mail.ru/osm/tools/overpass/api/interpreter"*)
+(*"https://overpass.private.coffee/api/interpreter"*)
+
+(*exception DownloadFailed of  Cohttp.Code.status_code*)
+
+(** downloads from uri to file with name dest*)
+let rec download (uri : Uri.t) (dest : string) =
+  let* resp, body = Client.get uri in
+  match resp.status with
+  | `OK -> let stream = Body.to_stream body in
   Lwt_io.with_file ~mode:Lwt_io.output dest (fun chan ->
       Lwt_stream.iter_s (Lwt_io.write chan) stream)
+  | _ -> print_endline "Retrying download"; download uri dest
+  
+(** downloads osm file of all buildings in area defined by query_relation*)
+let queryBuildings (query_relation : string) (admin_level : string)=
+  let url = api^"?data=relation%28"^query_relation^"%29%3Bmap%5Fto%5Farea%3Bnwr%5B%22building%22%5D%5B%22name%22%5D%5B%22addr%3Astreet%22%5D%28area%29%3Bout%20meta%3B%0A" in
+  download (Uri.of_string url) ("data/buildings/"^admin_level^"-"^query_relation^".osm")
 
-let queryBuildings (query_relation : string) =
-  let url = "https://overpass-api.de/api/interpreter?data=relation%28"^query_relation^"%29%3Bmap%5Fto%5Farea%3Bnwr%5B%22building%22%5D%5B%22name%22%5D%5B%22addr%3Astreet%22%5D%28area%29%3Bout%20meta%3B%0A" in
-  download (Uri.of_string url) ("data/buildings/"^query_relation^".osm")
-
+(** downloads osm file of all admin boundaries in area defined by query_relation. If there are no child boundaries, call queryBuildings*)
 let queryBoundaries (query_relation : string) (admin_level : string) =
-  let url = "https://overpass-api.de/api/interpreter?data=relation%28"^query_relation^"%29%3Bmap%5Fto%5Farea%3Brelation%5B%22boundary%22%3D%22administrative%22%5D%5B%22admin%5Flevel%22%21%3D%22"^admin_level^"%22%5D%28area%29%3Bout%20meta%3B%0A" in
-  Lwt.bind (download (Uri.of_string url) ("data/boundaries/"^query_relation^".osm")) (fun ()->
-    let osm = Osm_xml.Parser.parse_file ("data/boundaries/"^query_relation^".osm") in
+  let url = api^"?data=relation%28"^query_relation^"%29%3Bmap%5Fto%5Farea%3Brelation%5B%22boundary%22%3D%22administrative%22%5D%5B%22admin%5Flevel%22%21%3D%22"^admin_level^"%22%5D%28area%29%3Bout%20meta%3B%0A" in
+  Lwt.bind (download (Uri.of_string url) ("data/boundaries/"^admin_level^"-"^query_relation^".osm")) (fun ()->
+    let osm = Osm_xml.Parser.parse_file ("data/boundaries/"^admin_level^"-"^query_relation^".osm") in
     let relations_of_osm (Osm_xml.Types.OSM osm_record) = osm_record.relations in
     let relations = relations_of_osm osm in
     match Map.is_empty relations with
-    | true -> queryBuildings query_relation
+    | true -> queryBuildings query_relation admin_level
     | false -> Lwt.return_unit)
 
+(** downloads osm file of all admin boundaries in area defined by query_relation, and call queryBoundaries on all child boundaries*)
 let queryAllChildren (query_relation : string) (admin_level : string)=
-  let url = "https://overpass-api.de/api/interpreter?data=relation%28"^query_relation^"%29%3Bmap%5Fto%5Farea%3Brelation%5B%22boundary%22%3D%22administrative%22%5D%5B%22admin%5Flevel%22%21%3D%22"^admin_level^"%22%5D%28area%29%3Bout%20meta%3B%0A" in
-  Lwt_main.run (download (Uri.of_string url) ("data/boundaries/"^query_relation^".osm")); 
-  let osm = Osm_xml.Parser.parse_file ("data/boundaries/"^query_relation^".osm") in
+  let url = api^"?data=relation%28"^query_relation^"%29%3Bmap%5Fto%5Farea%3Brelation%5B%22boundary%22%3D%22administrative%22%5D%5B%22admin%5Flevel%22%21%3D%22"^admin_level^"%22%5D%28area%29%3Bout%20meta%3B%0A" in
+  Lwt_main.run (download (Uri.of_string url) ("data/boundaries/"^admin_level^"-"^query_relation^".osm")); 
+  let osm = Osm_xml.Parser.parse_file ("data/boundaries/"^admin_level^"-"^query_relation^".osm") in
   let relations_of_osm (Osm_xml.Types.OSM osm_record) = osm_record.relations in
   let relations = Map.to_alist (relations_of_osm osm) in
   match List.is_empty relations with
-  | true-> queryBuildings query_relation
-  | false-> Lwt_list.iter_p (fun ((Osm_xml.Types.OSMId child) ,Osm_xml.Types.OSMRelation osm_relation_record)-> let a_level= Osm_xml.Types.find_tag osm_relation_record.tags "admin_level" in
+  | true-> queryBuildings query_relation admin_level
+  | false-> Lwt_list.iter_s (fun ((Osm_xml.Types.OSMId child) ,Osm_xml.Types.OSMRelation osm_relation_record)-> let a_level= Osm_xml.Types.find_tag osm_relation_record.tags "admin_level" in
     match a_level with
     | None -> Lwt.return_unit
     | Some level -> queryBoundaries child level) relations
