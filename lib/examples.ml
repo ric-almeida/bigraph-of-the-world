cf30666e84a3c01410ca94f8bd31ab4648464bd1
Roy Ang <royangkr@gmail.com>
2025-03-08 23:37:04 +0000

    upgrade bigraph to 2.0.0

--- lib/examples.ml
+++ lib/examples.ml
@@ -1,12 +1,12 @@
 open Bigraph
 
-let agent = Big.ion (Link.parse_face ["connection"]) (Ctrl.C ("Agent", [], 1))
-let boundary = Big.ion (Link.Face.empty) (Ctrl.C ("Boundary", [], 0))
-let street = Big.ion (Link.Face.empty) (Ctrl.C ("Street", [], 0))
-let building = Big.ion (Link.Face.empty) (Ctrl.C ("Building", [], 0))
+let agent = Big.ion (Link.parse_face ["connection"]) Ctrl.{ s = "Agent"; p = []; i = 1 }
+let boundary = Big.ion (Link.Face.empty) Ctrl.{ s = "Boundary"; p = []; i = 0 }
+let street = Big.ion (Link.Face.empty) Ctrl.{ s = "Street"; p = []; i = 0 }
+let building = Big.ion (Link.Face.empty) Ctrl.{ s = "Building"; p = []; i = 0 }
 let site = Big.split 1
 let disconnected_agent = Big.close (Link.parse_face ["connection"]) agent
-let agent_a = Big.nest agent (Big.atom Link.Face.empty (Ctrl.C ("ID", [S "A"],0)))
+let agent_a = Big.nest agent (Big.atom Link.Face.empty Ctrl.{ s = "ID"; p = [S "A"]; i = 0 })
 
 module MSSolver = Solver.Make_SAT(Solver.MS)
 module BRS =  Brs.Make(MSSolver)
@@ -22,24 +22,24 @@ let write_state n b =
 let test = 
   Big.nest 
     boundary 
-    (Big.par_of_list [Big.atom Link.Face.empty (Ctrl.C ("ID", [S "6-180837-Cambridgeshire"], 0)); site ;(Big.nest 
+    (Big.par_of_list [Big.atom Link.Face.empty Ctrl.{ s = "ID"; p = [S "6-180837-Cambridgeshire"]; i = 0 }; site ;(Big.nest 
        boundary 
-       (Big.par_of_list [Big.atom Link.Face.empty (Ctrl.C ("ID", [S "8-295355-Cambridge"],0)); site ;(Big.nest 
+       (Big.par_of_list [Big.atom Link.Face.empty Ctrl.{ s = "ID"; p = [S "8-295355-Cambridge"]; i = 0 }; site ;(Big.nest 
           street 
-          (Big.par_of_list [Big.atom Link.Face.empty (Ctrl.C ("ID", [S "J J Thompson Avenue"],0)); site ; Big.nest
+          (Big.par_of_list [Big.atom Link.Face.empty Ctrl.{ s = "ID"; p = [S "J J Thompson Avenue"]; i = 0 }; site ; Big.nest
              building
              (Big.par 
                 site
-                (Big.atom Link.Face.empty (Ctrl.C ("ID", [S "8388-William Gates Building"], 0)))
-             )]
+                (Big.atom Link.Face.empty Ctrl.{ s = "ID"; p = [S "r8388-William Gates Building"]; i = 0 }))
+             ]
           ))]
        ))]
     );;
 
 let example_manual_change_lhs = 
-    Big.par (Big.nest building (Big.par site (Big.atom Link.Face.empty (Ctrl.C ("ID", [S "B"], 0))))) agent_a
+    Big.par (Big.nest building (Big.par site (Big.atom Link.Face.empty Ctrl.{ s = "ID"; p = [S "B"]; i = 0 }))) agent_a
 let react_example_manual_change = 
-    let rhs =  Big.nest building (Big.par_of_list [site; Big.atom Link.Face.empty (Ctrl.C ("ID", [S "B"], 0)); agent_a;]) in
+    let rhs =  Big.nest building (Big.par_of_list [site; Big.atom Link.Face.empty Ctrl.{ s = "ID"; p = [S "B"]; i = 0 }; agent_a;]) in
     BRS.parse_react_unsafe ~name:"Move down boundary to boundary"  ~lhs:example_manual_change_lhs ~rhs:rhs () None
 
 let example_connect_agents_lhs = Big.par disconnected_agent disconnected_agent
